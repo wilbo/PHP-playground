10474fa32bef130153fdb5b688322e76bcfcaee3
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2015-06-01 00:13:03 +0200

    init

---
+++ app/index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <title>FastShell by @_Karamii</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <link href="//www.google-analytics.com" rel="dns-prefetch">
+    <link href="//ajax.googleapis.com" rel="dns-prefetch">
+    <link href="assets/css/style.min.css" rel="stylesheet">
+
+    <script src="assets/components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+
+    <header class="header" role="banner">
+      <nav class="nav" role="navigation">
+        <!-- navigation -->
+      </nav>
+    </header>
+
+    <main class="main" role="main">
+      <div class="logo">
+        <h1>Just Play!</h1>
+      </div>
+    </main>
+
+    <footer class="footer" role="contentinfo">
+
+    </footer>
+
+    <script src="assets/components/jquery/jquery.min.js"></script>
+    <script src="assets/js/scripts.min.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
+
+  </body>
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    php = require('gulp-connect-php'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    header  = require('gulp-header'),
+    rename = require('gulp-rename'),
+    minifyCSS = require('gulp-minify-css'),
+    package = require('./package.json');
+
+
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
+
+gulp.task('css', function () {
+    return gulp.src('src/scss/style.scss')
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('js',function(){
+  gulp.src('src/js/scripts.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(uglify())
+    .pipe(header(banner, { package : package }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
+
+// gulp.task('browser-sync', function() {
+//     browserSync.init(null, {
+//         server: {
+//             baseDir: "app"
+//         }
+//     });
+// });
+
+var reload  = browserSync.reload;
+
+gulp.task('php', function() {
+    php.server({ base: 'app', port: 8010, keepalive: true});
+});
+gulp.task('browser-sync',['php'], function() {
+    browserSync({
+        proxy: 'gulp-playground.dev',
+        port: 8080,
+        open: true,
+        notify: false
+    });
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+    gulp.watch("src/scss/*/*.scss", ['css']);
+    gulp.watch("src/js/*.js", ['js']);
+    gulp.watch("app/*.php", ['bs-reload']);
+});
